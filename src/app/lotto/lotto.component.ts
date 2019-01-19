3b09260b19630157a4fe4808c2e76cb7eec44a2b
Angkarn Janjuang <angkarn9@gmail.com>
2019-01-19 12:41:55 +0700

    change style sheet type

--- src/app/lotto/lotto.component.ts
+++ src/app/lotto/lotto.component.ts
@@ -5,7 +5,7 @@ import { Dropdown } from './model/dropdown';
 @Component({
   selector: 'app-lotto',
   templateUrl: './lotto.component.html',
-  styleUrls: ['./lotto.component.sass']
+  styleUrls: ['./lotto.component.scss']
 })
 export class LottoComponent implements OnInit {
 
